4d2b9b7bad019fca4465b8ec542e4bcda525c90d
nastalique <nastaliquetariq7@gmail.com>
2022-09-11 23:23:52 +0500

    added Middleware

---
+++ docto/middleware/simple_middleware.py
@@ -0,0 +1,16 @@
+def simple_middleware(get_response):
+    # One-time configuration and initialization.
+
+    def middleware(request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = get_response(request)
+        response["Access-Control-Allow-Origin"] = "*"
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+    return middleware
